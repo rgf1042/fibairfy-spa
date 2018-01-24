1d2c1c95763f3df761e613f4e3871227abdc1b1e
Roger Garcia Ferre <roger.garcia@guifi.net>
2018-01-24 21:35:31 +0100

    New improvements: 	- Added style definition for class info (mouse over map)

--- src/components/FiberMap/leaflet-map.vue
+++ src/components/FiberMap/leaflet-map.vue
@@ -26,6 +26,8 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style>
   #map { height: 800px; }
+  .info {padding: 6px 8px; font: 16px; background: white; background: rgba(255,255,255,0.8); box-shadow: 0 0 15px rgba(0,0,0,0.2); border-radius: 5px; font-weight: bold; min-width: 100px}
+  .info h4 { margin: 0 0 5px; color: #777; }
 </style>
