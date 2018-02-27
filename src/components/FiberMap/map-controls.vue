5faac9c29d831c87f822ba2034a07990c5b99a84
Roger Garcia <roger.garcia@guifi.net>
2018-02-27 19:05:58 +0100

    New improvements: 	- Translations in catalan and english for map-controls

--- src/components/FiberMap/map-controls.vue
+++ src/components/FiberMap/map-controls.vue
@@ -12,22 +12,22 @@
     </b-modal>
     <b-row align-h="center">
       <b-button-toolbar key-nav  aria-label="Toolbar for map controlling" align-h="center" class="pt-1">
-        <b-input-group class="mx-1" prepend="Layer:">
+        <b-input-group class="mx-1" :prepend="this.$t('components.map.layerMenu.name')">
           <b-input-group-append>
-            <b-button variant="primary" :disabled="this.isCivil" @click="testActive({ layer: 'civil'}, $event)">Cívil</b-button>
-            <b-button variant="primary" :disabled="!this.isCivil" @click="testActive({ layer: 'infra'}, $event)">Xarxa</b-button>
+            <b-button variant="primary" :disabled="this.isCivil" @click="testActive({ layer: 'civil'}, $event)">{{$t('components.map.layerMenu.civil')}}</b-button>
+            <b-button variant="primary" :disabled="!this.isCivil" @click="testActive({ layer: 'infra'}, $event)">{{$t('components.map.layerMenu.network')}}</b-button>
           </b-input-group-append>
         </b-input-group>
-        <b-input-group class="mx-1" prepend="Obra Civil:" v-if="this.isCivil">
+        <b-input-group class="mx-1" :prepend="this.$t('components.map.civilMenu.name')" v-if="this.isCivil">
           <b-input-group-append>
-            <b-button :pressed="this.status === 'site'" @click="testActive({ status: 'site'}, $event)">Crea Site</b-button>
-            <b-button :pressed="this.status === 'path'" @click="testActive({ status: 'path'}, $event)">Crea Tram</b-button>
+            <b-button :pressed="this.status === 'site'" @click="testActive({ status: 'site'}, $event)">{{$t('components.map.civilMenu.newSite')}}</b-button>
+            <b-button :pressed="this.status === 'path'" @click="testActive({ status: 'path'}, $event)">{{$t('components.map.civilMenu.newPath')}}</b-button>
           </b-input-group-append>
         </b-input-group>
-        <b-input-group class="mx-1" prepend="Infraestructura xarxa:" v-if="!this.isCivil">
+        <b-input-group class="mx-1" :prepend="this.$t('components.map.networkMenu.name')" v-if="!this.isCivil">
           <b-input-group-append>
-            <b-button :pressed="this.status === 'box'" @click="testActive({ status: 'box'}, $event)">Crea Box</b-button>
-            <b-button :pressed="this.status === 'fiber'" @click="testActive({ status: 'fiber'}, $event)">Crea Fibra</b-button>
+            <b-button :pressed="this.status === 'box'" @click="testActive({ status: 'box'}, $event)">{{$t('components.map.networkMenu.newBox')}}</b-button>
+            <b-button :pressed="this.status === 'fiber'" @click="testActive({ status: 'fiber'}, $event)">{{$t('components.map.networkMenu.newFiber')}}</b-button>
           </b-input-group-append>
         </b-input-group>
         <b-button variant="info "v-if="this.status" @click="resetStatus($event)"><div v-html="octicons['arrow-left'].toSVG()"></div></b-button>
